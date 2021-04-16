3f267c3f4516719c00d06a6d31fa656dee32becf
Priya-raja <priya.thevar89@gmail.com>
2021-04-16 12:13:28 +0530

    Added components in index page. and styles

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import Head from 'next/head';
 
-export default function Home() {
+ function CompA() {
+  return (
+    <>
+     <h1>Comp A </h1>
+    <p>Hello dear Comp A</p>
+    <CompB/>
+    </>
+  )
+}
+function CompB() {
+  return (
+    <>
+     <h1>Comp B </h1>
+     <p>I'm also Comp B </p>
+    </>
+    
+  )
+}
+
+function Home() {
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <CompA />
+    </>
+    
   );
 }
+export default Home;
