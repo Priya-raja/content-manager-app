d9884b797dc478575ff2a02cadbff93646d99468
Priya-raja <priya.thevar89@gmail.com>
2021-04-17 14:18:35 +0530

    Stae and class function

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,35 @@
 import Head from 'next/head';
+import React, {useState} from 'react';
 
+
+const ArrowFunction = () => <h1>I am arrow </h1>
+  
  function CompA() {
   return (
     <>
+    <ArrowFunction/>
      <h1>Comp A </h1>
     <p>Hello dear Comp A</p>
-    <CompB/>
-    </>
-  )
-}
-function CompB() {
-  return (
-    <>
-     <h1>Comp B </h1>
-     <p>I'm also Comp B </p>
-    </>
     
+    </>
   )
 }
 
+
+//class
+ class CompC extends React.Component {
+   render() {
+     return(
+       <h1> Hello C</h1>
+     )
+   }
+ }
 function Home() {
+const [value, setValue] = useState(10)
 
   return (
     <>
+    {value}
       <h1>Hello World!</h1>
       <CompA />
     </>
