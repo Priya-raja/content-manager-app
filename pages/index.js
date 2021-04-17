ca757c37c5eb50dfa8224d49fd3c1de1724fe54b
Priya-raja <priya.thevar89@gmail.com>
2021-04-17 16:34:22 +0530

    Added props

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ import React, {useState} from 'react';
 
 const ArrowFunction = () => <h1>I am arrow </h1>
   
- function CompA() {
+ function CompA(props) {
   return (
     <>
     <ArrowFunction/>
      <h1>Comp A </h1>
     <p>Hello dear Comp A</p>
+    <p>this is value from Home {props.myProps}</p>
     
     </>
   )
@@ -17,27 +18,28 @@ const ArrowFunction = () => <h1>I am arrow </h1>
 
 
 //class
- class CompC extends React.Component {
-   render() {
-     return(
-       <h1> Hello C</h1>
-     )
-   }
- }
+//  class CompC extends React.Component {
+//    render() {
+//      return(
+//        <h1> Hello C</h1>
+//      )
+//    }
+//  }
 function Home() {
 const [value, setValue] = useState(10)
 console.log("Hi");
 const incrementValue =(incrementor) => {
   setValue(value + incrementor);
 }
-
   return (
     <>
     {value}
       <h1>Hello World!</h1>
       <button onClick={() => incrementValue(+1)}> + </button>
       <button onClick={() => incrementValue(-1)}> - </button>
-      <CompA />
+      <CompA
+       myProps= {value}
+      />
     </>
     
   );
